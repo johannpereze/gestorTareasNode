9c64674649b8dea007b55bdc8605ca964206e19a
johannpereze <sebas.jsp@gmail.com>
2021-09-20 17:42:25 -0500

    Clase 58 mi solucion de la tarea

--- app.js
+++ app.js
@@ -1,32 +1,44 @@
 require("colors");
 
 const { inquirerMenu, pausa, leerInput } = require("./helpers/inquirer");
-const Tareas = require("./helpers/models/tareas");
+const { guardarDB, leerDB } = require("./helpers/models/guardarArchivo");
+const { Tareas } = require("./helpers/models/tareas");
 
 console.clear();
 
 const main = async () => {
   let opt = "";
   const tareas = new Tareas();
+  const tareasDB = leerDB();
+
+  if (tareasDB) {
+    tareas.cargarTareasFromArr(tareasDB);
+    // console.log('log tarea',tarea);
+  }
 
   do {
     opt = await inquirerMenu();
-    // console.log(opt);
 
     switch (opt) {
-      case '1':
-        const desc = await leerInput('Descripción');
-        tareas.crearTarea(desc)
+      case "1":
+        const desc = await leerInput("Descripción");
+        tareas.crearTarea(desc);
         break;
-      case '2':
-        console.log(tareas._listado);
+      case "2":
+        tareas.listadoCompleto();
+        break;
+        case "3":
+        tareas.listarPendientesCompletadas(true);
+        break;
+        case "4":
+        tareas.listarPendientesCompletadas(false);
         break;
     }
 
+    guardarDB(tareas.listadoArr);
+
     await pausa();
   } while (opt !== "0");
-
-  // pause();
 };
 
 main();
--- helpers/models/tareas.js
+++ helpers/models/tareas.js
@@ -2,14 +2,58 @@ const Tarea = require("./tarea");
 
 class Tareas {
   _listado = {};
+
+  get listadoArr() {
+    const listado = [];
+
+    Object.keys(this._listado).forEach((key) => {
+      listado.push(this._listado[key]);
+    });
+
+    return listado;
+  }
+
   constructor() {
     this._listado = {};
+  }
+
+  cargarTareasFromArr(tareas = []) {
+    // console.log(tareas);
+    tareas.forEach((tarea) => {
+      this._listado[tarea.id] = tarea;
+    });
   }
 
   crearTarea(desc = "") {
     const tarea = new Tarea(desc);
     this._listado[tarea.id] = tarea;
   }
+
+  listadoCompleto() {
+    console.log();
+    this.listadoArr.forEach((tarea, index) => {
+      let element;
+      const i = `${index + 1}`.green;
+      tarea.completadoEn
+        ? (element = `${i}. ${tarea.desc} :: ${"Completada".green}`)
+        : (element = `${i}. ${tarea.desc} :: ${"Pendiente".red}`);
+      console.log(element);
+    });
+  }
+
+  listarPendientesCompletadas(completadas = true) {
+    console.log();
+
+    let idx = 1;
+
+    this.listadoArr.forEach((tarea) => {
+      if (Boolean(tarea.completadoEn) === completadas) {
+        const i = `${(idx)}`.green;
+        console.log(`${i}. ${tarea.desc}`);
+        idx++;
+      }
+    });
+  }
 }
 
-module.exports = Tareas;
+module.exports = { Tareas };
